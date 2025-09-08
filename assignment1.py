a64263c5e942d64264ca55461c4cbcc5ced9eb57
“Futam0m0” <limjunxianjedrek@gmail.com>
2025-09-08 23:45:00 +0900

    Completed my first python assignment

---
+++ assignment1.py
@@ -0,0 +1,28 @@
+# Question 1
+length = float(input("lenght: "))
+breadth = float (input("breadth: "))
+
+area = length*breadth
+
+print("The area of the rectangle is: ",area)
+
+# Question 2
+x = int(input("Enter integer: "))
+y = float(input("Enter float: "))
+
+z = x+y
+
+print("Sum: ",z)
+print("Data type is float as int is automatically converted to prevent loss of decimal")
+
+# Question 3
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+gender = input("Enter your gender: ")
+height = float(input("Enter your height: "))
+
+print("Details:")
+print("Name: ", name)
+print("Age: ", age)
+print("Gender: ", gender)
+print("Height: ", height)
